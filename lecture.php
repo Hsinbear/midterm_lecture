83f4f15b5ef6eed3d40b34080a378818d03e7a26
Hsin <vivopocoxiaomi@gmail.com>
2024-02-03 11:52:26 +0800

    修正UI

--- lecture.php
+++ lecture.php
@@ -206,35 +206,35 @@ if (isset($_GET["search"])) {
                         </li>
                         <li class="breadcrumb-item text-sm text-white active" aria-current="page">主頁面</li>
                     </ol>
-                    <!-- <h6 class="font-weight-bolder text-white mb-0">主頁面</h6> -->
-        </nav> -->
-        <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
-            <div class="ms-md-auto pe-md-3 d-flex align-items-center">
+                    <h6 class="font-weight-bolder text-white mb-0">主頁面</h6>
+                </nav> -->
+                <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
+                    <div class="ms-md-auto pe-md-3 d-flex align-items-center">
 
-            </div>
-            <div class="text-white px-4">
-                HI, USER
-            </div>
-            <ul class="navbar-nav  justify-content-end">
-                <li class="nav-item d-flex align-items-center">
-                    <a href="javascript:;" class="nav-link text-white font-weight-bold px-0">
-                        <i class="fa fa-user me-sm-1"></i>
-                        <span class="d-sm-inline d-none">登入</span>
-                    </a>
-                </li>
-                <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
-                    <a href="javascript:;" class="nav-link text-white p-0" id="iconNavbarSidenav">
-                        <div class="sidenav-toggler-inner">
-                            <i class="sidenav-toggler-line bg-white"></i>
-                            <i class="sidenav-toggler-line bg-white"></i>
-                            <i class="sidenav-toggler-line bg-white"></i>
-                        </div>
-                    </a>
-                </li>
+                    </div>
+                    <div class="text-white px-4">
+                        HI, USER
+                    </div>
+                    <ul class="navbar-nav  justify-content-end">
+                        <li class="nav-item d-flex align-items-center">
+                            <a href="javascript:;" class="nav-link text-white font-weight-bold px-0">
+                                <i class="fa fa-user me-sm-1"></i>
+                                <span class="d-sm-inline d-none">登入</span>
+                            </a>
+                        </li>
+                        <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
+                            <a href="javascript:;" class="nav-link text-white p-0" id="iconNavbarSidenav">
+                                <div class="sidenav-toggler-inner">
+                                    <i class="sidenav-toggler-line bg-white"></i>
+                                    <i class="sidenav-toggler-line bg-white"></i>
+                                    <i class="sidenav-toggler-line bg-white"></i>
+                                </div>
+                            </a>
+                        </li>
 
-            </ul>
-        </div>
-        </div>
+                    </ul>
+                </div>
+            </div>
         </nav>
         <!-- End Navbar -->
         <div class="container-fluid py-4">
